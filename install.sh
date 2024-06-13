f7d07cd737f6322e8c82ea8ee83c06ed0d9ea8ae
Hugo Sibony <hugo.sibony@epita.fr>
2024-06-14 01:06:25 +0200

    changed check

--- install.sh
+++ install.sh
@@ -9,44 +9,77 @@ NC='\033[0m' # No Color
 # List of packages to be installed
 packagesNeeded="vim zsh git curl"
 
+# Function to check if a package is installed
+package_installed() {
+    package=$1
+    case $package_manager in
+        apk) apk info $package >/dev/null 2>&1 ;;
+        apt-get) dpkg -l | grep -w "^ii  $package" >/dev/null 2>&1 ;;
+        dnf) rpm -q $package >/dev/null 2>&1 ;;
+        zypper) rpm -q $package >/dev/null 2>&1 ;;
+        pacman) pacman -Qs $package >/dev/null 2>&1 ;;
+        *) return 1 ;;
+    esac
+}
+
 # Function to install packages based on package manager
 install_packages() {
     package_manager=$1
-    echo -e "${YELLOW}Installing packages using $package_manager...${NC}"
-    case $package_manager in
-        apk) sudo apk add --no-cache $packagesNeeded ;;
-        apt-get) sudo apt-get update && sudo apt-get install -y $packagesNeeded ;;
-        dnf) sudo dnf install -y $packagesNeeded ;;
-        zypper) sudo zypper install -y $packagesNeeded ;;
-        pacman) sudo pacman -Syu --noconfirm $packagesNeeded ;;
-        *) echo -e "${RED}FAILED TO INSTALL PACKAGES: Unsupported package manager.${NC}" >&2; return 1 ;;
-    esac
-    echo -e "${GREEN}Packages installed successfully.${NC}"
+    printf "${YELLOW}Installing packages using $package_manager...${NC}\n"
+    for package in $packagesNeeded; do
+        if ! package_installed $package; then
+            case $package_manager in
+                apk) sudo apk add --no-cache $package ;;
+                apt-get) sudo apt-get update && sudo apt-get install -y $package ;;
+                dnf) sudo dnf install -y $package ;;
+                zypper) sudo zypper install -y $package ;;
+                pacman) sudo pacman -Syu --noconfirm $package ;;
+                *) printf "${RED}FAILED TO INSTALL $package: Unsupported package manager.${NC}\n" >&2; return 1 ;;
+            esac
+        else
+            printf "${GREEN}$package is already installed.${NC}\n"
+        fi
+    done
+    printf "${GREEN}Packages installed successfully.${NC}\n"
 }
 
 # Function to change the default shell
 change_default_shell() {
     shell=$1
     if command -v $shell >/dev/null 2>&1; then
-        echo -e "${YELLOW}Changing default shell to $shell...${NC}"
-        chsh -s "$(command -v $shell)"
-        echo -e "${GREEN}Default shell changed to $shell.${NC}"
+        if [ "$SHELL" != "$(command -v $shell)" ]; then
+            printf "${YELLOW}Changing default shell to $shell...${NC}\n"
+            chsh -s "$(command -v $shell)"
+            printf "${GREEN}Default shell changed to $shell.${NC}\n"
+        else
+            printf "${GREEN}Default shell is already set to $shell.${NC}\n"
+        fi
     else
-        echo -e "${RED}Shell $shell not found.${NC}" >&2
+        printf "${RED}Shell $shell not found.${NC}\n" >&2
         return 1
     fi
 }
 
-# Function to clone repositories or download files
+# Function to check if a directory exists
+directory_exists() {
+    directory=$1
+    [ -d "$directory" ]
+}
+
+# Function to clone repositories or download files if not already present
 fetch_and_install() {
     url=$1
     target=$2
-    echo -e "${YELLOW}Fetching $url and installing to $target...${NC}"
-    if git clone --quiet $url $target; then
-        echo -e "${GREEN}Installation to $target completed.${NC}"
+    printf "${YELLOW}Fetching $url and installing to $target...${NC}\n"
+    if ! directory_exists $target; then
+        if git clone --quiet $url $target; then
+            printf "${GREEN}Installation to $target completed.${NC}\n"
+        else
+            printf "${RED}Failed to fetch $url.${NC}\n" >&2
+            return 1
+        fi
     else
-        echo -e "${RED}Failed to fetch $url.${NC}" >&2
-        return 1
+        printf "${GREEN}$target already exists.${NC}\n"
     fi
 }
 
@@ -68,24 +101,28 @@ main() {
 
     # Download and update .vimrc
     curl -fsSL https://raw.githubusercontent.com/KazeTachinuu/config/master/.vimrc -o $HOME/.vimrc
-    echo -e "${YELLOW}vimrc updated.${NC}"
+    printf "${YELLOW}vimrc updated.${NC}\n"
 
     # Install Oh-My-Zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed '/\s*exec\s\s*zsh\s*-l\s*/d')" --unattended --skip-chsh || true
 
     # Download and update .zshrc
     curl -fsSL https://raw.githubusercontent.com/KazeTachinuu/config/master/.zshrc -o $HOME/.zshrc
-    echo -e "${YELLOW}zshrc updated.${NC}"
+    printf "${YELLOW}zshrc updated.${NC}\n"
 
     # Install zsh plugins (zsh-autosuggestions and zsh-syntax-highlighting)
     fetch_and_install https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions || true
     fetch_and_install https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.local/share/zsh/plugins/zsh-syntax-highlighting || true
 
     # Install useful aliases
-    fetch_and_install https://raw.githubusercontent.com/KazeTachinuu/config/master/.my_aliases.txt $HOME/.my_aliases.txt || true
+    if [ ! -f "$HOME/.my_aliases.txt" ]; then
+        fetch_and_install https://raw.githubusercontent.com/KazeTachinuu/config/master/.my_aliases.txt $HOME/.my_aliases.txt || true
+    else
+        printf "${YELLOW}File $HOME/.my_aliases.txt already exists and is not empty.${NC}\n"
+    fi
 
     # Notify completion
-    echo -e "${GREEN}All tasks completed successfully.${NC}"
+    printf "${GREEN}All tasks completed successfully.${NC}\n"
     env zsh  # Start zsh shell
 }
 
